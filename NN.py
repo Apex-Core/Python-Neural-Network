6c5cb6d15d67554dbdb88d405b67311f3eff2d16
Regin Hurley David <reginhurleydavid@MacBook-Air-2.local>
2022-01-28 10:32:11 -0700

    comparing output with expected output and scoring them

--- NN.py
+++ NN.py
@@ -6,6 +6,19 @@ X = [[1.0, 2.0, 3.0, 2.5],
           [2.0, 5.0, -1.0, 2.0],
           [-1.5, 2.7, 3.3, -0.8]]
 
+# Y = [[18,   -29],
+#  [0.14100315, -5],
+#  [4, -3]]
+
+Y = [[0.148296,   -0.08397602],
+ [0.14100315, -0.01340469],
+ [0.20124979, -0.07290616]]
+
+correctValues = [0, 0, 0, 0, 0, 0]
+amountCorrect = 0
+amountOfVariables = 6;
+percentCorrect = 0
+
 class LayerDense:
     def __init__ (self, inputCount, neronCount):
         self.weights = 0.10 * np.random.randn(inputCount, neronCount)
@@ -21,3 +34,21 @@ layer1.forward(X)
 layer2.forward(layer1.output)
 
 print(layer2.output)
+
+def compareValues():
+    currentIndex = 0
+
+    for i in range(0, 3):
+        for j in range(0, 2):
+            if layer2.output[i][j] - Y[i][j] <= 0.5 and layer2.output[i][j] - Y[i][j] >= -0.5:
+                correctValues[currentIndex] = 1
+                currentIndex += 1
+
+
+compareValues()
+
+amountCorrect = correctValues[0] + correctValues[1] + correctValues[2] + correctValues[3] + correctValues[4] + correctValues[5]
+percentCorrect = (amountCorrect / amountOfVariables) * 100
+
+print(percentCorrect)
+print(correctValues)
